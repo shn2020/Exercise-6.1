3e8033971cba7628fa475156589556a6785ba542
Sang Nguyen <shn2020@email.vccs.edu>
2016-04-18 15:10:03 -0400

    first commit

---
+++ Calculator.java
@@ -0,0 +1,41 @@
+public class Calculator {
+	public static double getSum(double a, double b) {
+		return a+b;
+	}
+	
+	public static double getSum(double[] num) {
+		double sum = 0;
+		for (double e: num) {
+			sum += e;
+		}
+		return sum;
+	}
+	
+	public static double getProduct(double a, double b) {
+		return a*b;
+	}
+	
+	public static double getProduct(double[] num) {
+		double product = 1;
+		for (double e: num) {
+			product *= e;
+		}
+		return product;
+	}
+	
+	public static double getAverage(double a, double b) {
+		return getSum(a,b) / 2.0;
+	}
+	
+	public static double getAverage(double[] num) {
+		return getSum(num) / num.length;
+	}
+	
+	public static double getFactorial(double a) {
+		double factorial = 1.0;
+		for (double i = 1; i <= a ; i++) {
+			factorial *= i;
+		}
+		return factorial;
+	}
+}
---
+++ testCalculator.java
@@ -0,0 +1,22 @@
+
+public class testCalculator {
+	public static void main(String[] args) {
+		double[] num = new double[5];
+		num[0] = 1.0;
+		num[1] = 2.0;
+		num[2] = 3.0;
+		num[3] = 4.0;
+		num[4] = 5.0;
+		
+		System.out.println(num[1] + " + " + num [2] + " = " +Calculator.getSum(num[1],num[2]));
+		System.out.println("Sum of array = " + Calculator.getSum(num));
+		
+		System.out.println("(" + num[1] + " + " + num [2] +")/ 2 = " +Calculator.getAverage(num[1],num[2]));
+		System.out.println("Average of array = " + Calculator.getAverage(num));
+		
+		System.out.println(num[1] + " * " + num [2] + " = " + Calculator.getProduct(num[1],num[2]));
+		System.out.println("Product of array = " + Calculator.getProduct(num));
+		
+		System.out.println((int)num[4] + "! = " + (int)Calculator.getFactorial(num[4]));
+	}
+}
